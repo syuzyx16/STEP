5bd68d0f7603db4e78c396a1c115fca63353c5c8
Yix Zhu <syuzyx16@gmail.com>
2025-06-06 14:51:56 +0900

    update

---
+++ week2/hw01_update.py
@@ -0,0 +1,204 @@
+import random, sys, time
+
+def calculate_hash(key):
+    assert type(key) == str 
+    hash = 0
+    primes = [23,29,31,37,41,43,47,53,59,61]
+    for i,ch in enumerate(key):
+        hash = ord(ch) + hash* primes[i%len(primes)] 
+    
+    return hash
+
+class Item:
+    def __init__(self, key, value, next):
+        assert type(key) == str
+        self.key = key
+        self.value = value
+        self.next = next
+
+class HashTable:
+
+    # Initialize the hash table.
+    def __init__(self):
+        self.bucket_size = 97
+        self.buckets = [None] * self.bucket_size  # [None, None, None, ...]
+        self.item_count = 0
+
+    def put(self, key, value):
+        assert type(key) == str
+        self.check_size() # Note: Don't remove this code.
+        bucket_index = calculate_hash(key) % self.bucket_size
+        # buckets contain lists so when item = buckets[index] 
+        # links to the head of one of the list
+        item = self.buckets[bucket_index]  
+        while item:
+            if item.key == key:
+                item.value = value
+                return False
+            item = item.next
+        new_item = Item(key, value, self.buckets[bucket_index])
+        self.buckets[bucket_index] = new_item
+        self.item_count += 1
+        if self.bucket_size < self.item_count:
+            self.expand()
+        
+        return True
+
+    
+    def get(self, key):
+        assert type(key) == str
+        self.check_size() # Note: Don't remove this code.
+        bucket_index = calculate_hash(key) % self.bucket_size
+        item = self.buckets[bucket_index]
+        while item:
+            if item.key == key:
+                return (item.value, True)
+            item = item.next
+        return (None, False)
+
+    
+    def delete(self, key):
+        assert type(key) == str
+        bucket_index = calculate_hash(key) % self.bucket_size
+        item = self.buckets[bucket_index] 
+        
+        prev = None
+        while item: 
+            if item.key == key:
+                if prev == None:
+                    self.buckets[bucket_index] = item.next
+                else:
+                    prev.next = item.next
+                self.item_count -= 1
+                return True
+            prev = item
+            item = item.next
+            
+        return False
+                
+   #rehash     
+
+    def expand(self):
+        old_buckets = self.buckets
+        self.bucket_size = self.bucket_size *2 +1
+        self.buckets = [None] * self.bucket_size
+        
+        for old_item in old_buckets : # old item is the head of list in bucket...
+          
+            while old_item:
+                bucket_index = calculate_hash(old_item.key) % self.bucket_size
+                new_item = Item(old_item.key, old_item.value, None)
+                current = self.buckets[bucket_index]   
+                if self.buckets[bucket_index] == None:
+                    self.buckets[bucket_index] = new_item
+                else:
+                    while current.next:
+                        current = current.next
+                    current.next = new_item
+                old_item = old_item.next
+        
+  
+    def size(self):
+        return self.item_count
+
+   
+    def check_size(self):
+        assert (self.bucket_size < 100 or
+                self.item_count >= self.bucket_size * 0.3)
+
+
+def functional_test():
+    hash_table = HashTable()
+
+    assert hash_table.put("aaa", 1) == True
+    assert hash_table.get("aaa") == (1, True)
+    assert hash_table.size() == 1
+
+    assert hash_table.put("bbb", 2) == True
+    assert hash_table.put("ccc", 3) == True
+    assert hash_table.put("ddd", 4) == True
+    assert hash_table.get("aaa") == (1, True)
+    assert hash_table.get("bbb") == (2, True)
+    assert hash_table.get("ccc") == (3, True)
+    assert hash_table.get("ddd") == (4, True)
+    assert hash_table.get("a") == (None, False)
+    assert hash_table.get("aa") == (None, False)
+    assert hash_table.get("aaaa") == (None, False)
+    assert hash_table.size() == 4
+
+    assert hash_table.put("aaa", 11) == False
+    assert hash_table.get("aaa") == (11, True)
+    assert hash_table.size() == 4
+
+    assert hash_table.delete("aaa") == True
+    assert hash_table.get("aaa") == (None, False)
+    assert hash_table.size() == 3
+
+    assert hash_table.delete("a") == False
+    assert hash_table.delete("aa") == False
+    assert hash_table.delete("aaa") == False
+    assert hash_table.delete("aaaa") == False
+
+    assert hash_table.delete("ddd") == True
+    assert hash_table.delete("ccc") == True
+    assert hash_table.delete("bbb") == True
+    assert hash_table.get("aaa") == (None, False)
+    assert hash_table.get("bbb") == (None, False)
+    assert hash_table.get("ccc") == (None, False)
+    assert hash_table.get("ddd") == (None, False)
+    assert hash_table.size() == 0
+
+    assert hash_table.put("abc", 1) == True
+    assert hash_table.put("acb", 2) == True
+    assert hash_table.put("bac", 3) == True
+    assert hash_table.put("bca", 4) == True
+    assert hash_table.put("cab", 5) == True
+    assert hash_table.put("cba", 6) == True
+    assert hash_table.get("abc") == (1, True)
+    assert hash_table.get("acb") == (2, True)
+    assert hash_table.get("bac") == (3, True)
+    assert hash_table.get("bca") == (4, True)
+    assert hash_table.get("cab") == (5, True)
+    assert hash_table.get("cba") == (6, True)
+    assert hash_table.size() == 6
+
+    assert hash_table.delete("abc") == True
+    assert hash_table.delete("cba") == True
+    assert hash_table.delete("bac") == True
+    assert hash_table.delete("bca") == True
+    assert hash_table.delete("acb") == True
+    assert hash_table.delete("cab") == True
+    assert hash_table.size() == 0
+    print("Functional tests passed!")
+
+
+
+def performance_test():
+    hash_table = HashTable()
+
+    for iteration in range(100):
+        begin = time.time()
+        random.seed(iteration)
+        for i in range(10000):
+            rand = random.randint(0, 100000000)
+            hash_table.put(str(rand), str(rand))
+        random.seed(iteration)
+        for i in range(10000):
+            rand = random.randint(0, 100000000)
+            hash_table.get(str(rand))
+        end = time.time()
+        print("%d %.6f" % (iteration, end - begin))
+
+    for iteration in range(100):
+        random.seed(iteration)
+        for i in range(10000):
+            rand = random.randint(0, 100000000)
+            hash_table.delete(str(rand))
+
+    assert hash_table.size() == 0
+    print("Performance tests passed!")
+
+
+if __name__ == "__main__":
+    functional_test()
+    performance_test()
